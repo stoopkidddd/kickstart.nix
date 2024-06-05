1be122633cf41ab8490417fa60a0d1940567ec1e
mattd-tg <mattd@twingate.com>
2024-06-05 19:11:04 -0400

    add fzf-lua, zen, disabled chatgpt

--- module/nvim/lua/config/keymaps.lua
+++ module/nvim/lua/config/keymaps.lua
@@ -3,11 +3,27 @@
 -- Add any additional keymaps here
 vim.keymap.set("n", "<leader>T", function()
   require("toggleterm").toggle()
-end)
+end, { desc = "Toggle Term" })
 
 vim.keymap.set("n", "<leader>yga", function()
   require("toggleterm").exec("yarn graphql:all", 1, 0.3, vim.fn.getcwd())
-end, {})
+end, { desc = "Generate GraphQL" })
 
-vim.api.nvim_set_keymap("n", "<leader><space>", "<cmd>Telescope smart_open<CR>", {})
-vim.api.nvim_set_keymap("n", "<leader>sf", "<cmd>Telescope smart_open<CR>", {})
+vim.keymap.set("n", "<leader><space>", "<cmd>Telescope smart_open<CR>", {})
+vim.keymap.set("n", "<leader>sf", "<cmd>Telescope smart_open<CR>", { desc = "Find Files" })
+
+-- vim.keymap.set("n", "<leader>CC", "<cmd>ChatGPT<CR>", { desc = "ChatGPT" })
+
+vim.keymap.set("n", "<leader>bp", "<cmd>bp<CR>", { desc = "Previous Buffer" })
+vim.keymap.set("n", "<leader>bn", "<cmd>bn<CR>", { desc = "Next Buffer" })
+
+vim.keymap.set("n", "<leader>z", "<cmd>ZenMode<CR>", { desc = "Zen Mode" })
+
+vim.keymap.set(
+  "n",
+  "<leader>/",
+  '<cmd>lua require("fzf-lua").live_grep_glob({fzf_layout = "reverse-list"})<cr>',
+  { desc = "Live Grep Glob" }
+)
+
+vim.keymap.set("n", "<leader>fz", "<cmd>FzfLua<CR>", { desc = "Fzf Lua" })
---
+++ module/nvim/lua/plugins/chatgpt.lua
@@ -0,0 +1,18 @@
+return {}
+
+-- return {
+--   "jackMort/ChatGPT.nvim",
+--   event = "VeryLazy",
+--   opts = {
+--     api_key_cmd = "op read op://Employee/OpenAI/credential --no-newline",
+--   },
+--   -- config = function()
+--   --   require("chatgpt").setup()
+--   -- end,
+--   dependencies = {
+--     "MunifTanjim/nui.nvim",
+--     "nvim-lua/plenary.nvim",
+--     "folke/trouble.nvim",
+--     "nvim-telescope/telescope.nvim",
+--   },
+-- }
