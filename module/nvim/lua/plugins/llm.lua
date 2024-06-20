67a5921bf956140f1b7e328be32f7dd3758f7188
mattd-tg <mattd@twingate.com>
2024-06-20 11:50:44 -0400

    add diffview - disabled llm.nvim

---
+++ module/nvim/lua/plugins/llm.lua
@@ -0,0 +1,22 @@
+return {}
+
+-- works but crappy output
+
+-- return {
+--   "huggingface/llm.nvim",
+--   opts = {
+--     model = "llama3:latest",
+--
+--     backend = "ollama",
+--     url = "http://192.168.1.164:11434", -- llm-ls uses "/api/generate"
+--
+--     -- cf https://github.com/ollama/ollama/blob/main/docs/api.md#parameters
+--     request_body = {
+--       -- Modelfile options for the model you use
+--       options = {
+--         temperature = 0.2,
+--         top_p = 0.95,
+--       },
+--     },
+--   },
+-- }
