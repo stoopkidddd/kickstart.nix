d312c7874f7da000b011ee978f9fa304c0c1c164
mattd-tg <mattd@twingate.com>
2024-06-27 11:03:41 -0400

    remove snippets

---
+++ module/nvim/lua/plugins/friendly-snippets.lua
@@ -0,0 +1,8 @@
+return {
+  "rafamadriz/friendly-snippets",
+  -- I hate snippets
+  enabled = false,
+  -- config = function()
+  --   require("luasnip.loaders.from_vscode").lazy_load()
+  -- end,
+}
